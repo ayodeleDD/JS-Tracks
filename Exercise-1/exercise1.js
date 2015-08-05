39067081ca1a7aa0649ff03a416f452d042e7de1
Ayodele Opetumo <ayodele.opetumo@dealdey.com>
2015-08-05 11:47:43 +0100

    Fixed issues with User Class in Exercise 1

--- Exercise-1/exercise1.js
+++ Exercise-1/exercise1.js
@@ -3,23 +3,25 @@
 class User {
   constructor(fullName, age) {
     this.fullName = fullName;
-    this.age = age;
+    this.age = parseInt(age, 10);
   }
 
-  compare(age) {
-    let text = '';
-    const userA = parseInt(this.age);
-    const userB = parseInt(age);
+  compare(user2) {
+    let ageDiff;
 
-    let comparison = (userA > userB) ? 
-      (text = user1.fullName + ' is older than ' + user2.fullName) : 
-      (text = user2.fullName + ' is older than ' + user1.fullName);
+    if (this.age > user2.age) {
+      ageDiff = `${this.fullName} is older than ${user2.fullName}`;
+    } else if (this.age === user2.age) {
+      ageDiff = `${user2.fullName} and ${this.fullName} are same age`;
+    } else {
+      ageDiff = `${user2.fullName} is older than ${this.fullName}`;
+    }
 
-    document.getElementById('age-result').innerHTML = comparison;
+    console.log(ageDiff);
   }
 }
 
-const user1 = new User('John Doe', '9');
-const user2 = new User('Mary Abu', '30');
+const user1 = new User('John', '35');
+const user2 = new User('Mary', '025');
 
-user1.compare(user2.age);
+user1.compare(user2);
