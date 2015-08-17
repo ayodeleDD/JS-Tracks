b62536488dbe7ca1d441990eb8b1f00796e3607e
Ayodele Opetumo <ayodele.opetumo@dealdey.com>
2015-08-17 11:47:32 +0100

    Fix issues with Exercise 3

--- Exercise-3/weekdays.js
+++ Exercise-3/weekdays.js
@@ -21,51 +21,45 @@ class ShowChecked {
     const _this = this;
     if (formElement.type) {
       if (formElement.checked) {
-        _this.verifyMax(formElement, this.checkBoxLimit);
-      } else {
-        _this.removeUnchecked(formElement);
+        if (formElement.id === 'none') {
+          _this.clearChecked();
+        } else {
+          _this.verifyMax(formElement, this.checkBoxLimit);
+        }
       }
     }
   }
 
   verifyMax(formElement, checkBoxLimit) {
     const _this = this;
-    const checkBoxNone = document.getElementById('none');
-    if (formElement.id === 'none') {
-      _this.clearChecked();
-    } else {
-      checkBoxNone.checked = false;
-      if (this.checkBoxArray.length >= this.checkBoxLimit) {
-        _this.showWarning(formElement, this.checkBoxLimit);
-      } else {
-        this.checkBoxArray.push(formElement);
-      }
+    const none = document.getElementById('none');
+    if (none.checked === true) {
+      none.checked = false;
+    }
+    const validCheckedBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    if (validCheckedBoxes.length > this.checkBoxLimit) {
+      _this.showWarning(formElement);
     }
   }
 
-  showWarning(formElement, checkBoxLimit) {
-    this.checkBoxArray.length = checkBoxLimit;
+  showWarning(formElement) {
     formElement.checked = false;
-    let days = [];
-    for (let i = this.checkBoxArray.length - 1; i >= 0; i--) {
-      days.push(this.checkBoxArray[i].value);
-    };
-    alert(`Only ${checkBoxLimit} days can be selected. You have already selected ${days.join(', ')}`);
+    const validCheckedBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    let validCheckedValue = '';
+    if (formElement.checked === false) {
+      for (let i = 0; i < validCheckedBoxes.length; i++)
+        validCheckedValue += `${validCheckedBoxes[i].value} `;
+    }
+    alert(`Only ${this.checkBoxLimit} days can be selected. You have already selected ${validCheckedValue}`);
   }
 
   clearChecked() {
     const weekdayCheckBox = document.getElementsByClassName('weekday');
     for (let i = weekdayCheckBox.length - 1; i >= 0; i--) {
       weekdayCheckBox[i].checked = false;
-    };
-    this.checkBoxArray = [];  //Resets Array
-  }
-
-  removeUnchecked(formElement) {
-    const indexOfBox = this.checkBoxArray.indexOf(formElement);
-    this.checkBoxArray.splice(indexOfBox, 1); //Remove Unchecked Checkbox
+    }
   }
 }
 
 const form = document.getElementById('myform');
-new ShowChecked(form, 3);
+new ShowChecked(form, 4);
