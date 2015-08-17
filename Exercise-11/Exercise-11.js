736b400bd309f69baee1f40e81debfba77f30fb2
Ayodele Opetumo <ayodele.opetumo@dealdey.com>
2015-08-17 14:59:04 +0100

    Fix issue with Exercise 11 JS

--- Exercise-11/Exercise-11.js
+++ Exercise-11/Exercise-11.js
@@ -9,28 +9,37 @@ class NumberValidator {
   init() {
     const _this = this;
     const numberPattern = /^[0-9]*(?:\.\d{1,2})?$/;
-    const numberTextField = document.getElementById('number');
-    const resultTextField = document.getElementById('result');
     this.myForm.addEventListener('submit', function(formEvent) {
-      _this.validateNumber(formEvent, numberPattern, numberTextField, resultTextField);
+      _this.validateField(formEvent, numberPattern);
     });
   }
 
-  validateNumber(formEvent, numberPattern, numberTextField, resultTextField) {
-    if (!numberPattern.test(numberTextField.value)) {
-      resultTextField.value = 'False';
-      numberTextField.focus();
-      formEvent.preventDefault();
-    } else if (numberTextField.value === '') {
+  validateField(formEvent, numberPattern) {
+    const _this = this;
+    const numberTextField = document.getElementById('number');
+    const resultTextField = document.getElementById('result');
+    if (numberTextField.value.trim() === '') {
       numberTextField.focus();
       resultTextField.value = 'Nothing to validate';
+      formEvent.preventDefault();
+    } else {
+      _this.validateNumber(formEvent, numberPattern);
+    }
+  }
+
+  validateNumber(formEvent, numberPattern) {
+    const numberTextField = document.getElementById('number');
+    const resultTextField = document.getElementById('result');
+    if (!numberPattern.test(numberTextField.value.trim())) {
+      resultTextField.value = 'False';
+      numberTextField.focus();
       formEvent.preventDefault();
     } else {
       resultTextField.value = 'True';
       formEvent.preventDefault();
       setTimeout(function () {
         this.myForm.submit();
-      }, 5000); //setTimeout delays submission of form for 5 seconds to show result value
+      }, 10000); //setTimeout delays submission of form for 10 seconds to show result value
     }
   }
 }
